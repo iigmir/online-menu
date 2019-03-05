c17657ba80418d3cc6c018408b8046ed38a07ab2
iigmir <roc120j@gmail.com>
2019-03-05 20:44:32 +0800

    Test compelete

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -1,9 +1,11 @@
 import { expect } from "chai";
-import { shallowMount, mount } from "@vue/test-utils"; // shallowMount,
+import { shallowMount } from "@vue/test-utils"; // shallowMount,
 import Home from "../../src/views/Home.vue";
 
-describe("Home.vue", () => {
-    it("lets_checkout always change in_checkout to Boolean type", () => {
+describe("Home.vue", () =>
+{
+    it("lets_checkout always change in_checkout to Boolean type", () =>
+    {
         let test_cases = [
             { input: true, output: true },
             { input: false, output: false },
@@ -15,15 +17,12 @@ describe("Home.vue", () => {
             { input: [], output: false },
             { input: {}, output: false }
         ];
-        const wrapper = shallowMount(Home, {});
-        const component = wrapper.vm;
-        component.lets_checkout(test_cases[0].input);
-        expect(component.in_checkout).to.equal(test_cases[0].output);
-        // test_cases.forEach( case => {
-        //     const wrapper = shallowMount(Home, {});
-        //     const component = wrapper.vm;
-        //     component.lets_checkout(case.input);
-        //     expect(component.in_checkout).to.equal(case.output);
-        // });
+        test_cases.forEach( this_case =>
+        {
+            const wrapper = shallowMount(Home, {});
+            const component = wrapper.vm;
+            component.lets_checkout( this_case.input );
+            expect( component.in_checkout ).to.equal( this_case.output );
+        });
     })
 });
